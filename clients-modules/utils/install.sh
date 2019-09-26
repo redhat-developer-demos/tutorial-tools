6c23fd338c0aafcc122add4431701829c7cc9b77
Kamesh Sampath <kamesh.sampath@hotmail.com>
2019-09-26 14:10:00 +0800

    add stern and hey to the util tools

--- clients-modules/utils/install.sh
+++ clients-modules/utils/install.sh
@@ -11,9 +11,10 @@ SOURCES_DIR=/tmp/artifacts
 cp ${SOURCES_DIR}/yq /usr/local/bin/yq
 cp ${SOURCES_DIR}/jq /usr/local/bin/jq
 cp ${SOURCES_DIR}/stern /usr/local/bin/stern
+cp ${SOURCES_DIR}/hey /usr/local/bin/hey
 
 cp -v ${SCRIPT_DIR}/bin/run.sh /usr/local/bin/
 
 chmod +x /usr/local/bin/run.sh
 
-chmod +x /usr/local/bin/yq /usr/local/bin/jq /usr/local/bin/stern
+chmod +x /usr/local/bin/yq /usr/local/bin/jq /usr/local/bin/stern /usr/local/bin/hey
