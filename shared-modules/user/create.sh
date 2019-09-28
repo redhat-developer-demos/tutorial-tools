b09b0581fe9da564950a33420e545149fa282cd1
Kamesh Sampath <kamesh.sampath@hotmail.com>
2019-09-28 13:09:58 +0800

    (fix) entrypoint and environment variable

--- shared-modules/user/create.sh
+++ shared-modules/user/create.sh
@@ -2,7 +2,16 @@
 
 set -eu
 
-useradd -u 1001 -r -g 0 -m -d "${DEVELOPER_HOME}" -s /sbin/nologin -c "A Developer" "${USER_NAME}"
+SCRIPT_DIR=$(dirname $0)
+
+cp -v ${SCRIPT_DIR}/entrypoint.sh /entrypoint.sh
+chown 0:0 /entrypoint.sh
+
+mkdir -p ${DEVELOPER_HOME} \
+  && chgrp -R 0 /home \
+  && chmod -R g=u /etc/passwd /home \
+  && chmod +x /entrypoint.sh
+
 chgrp -R 0 "${DEVELOPER_HOME}"
 chmod -R g=u "${DEVELOPER_HOME}"
 
