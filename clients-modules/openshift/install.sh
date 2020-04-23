9ad55487cca2613b67408528d61aedf5820c0d26
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-04-23 23:04:05 +0530

    version bumps and jdk 11
    - opeshift clients
    - graal
    - maven

--- clients-modules/openshift/install.sh
+++ clients-modules/openshift/install.sh
@@ -7,7 +7,6 @@ set -o pipefail
 
 SOURCES_DIR=/tmp/artifacts
 
-cp -v ${SOURCES_DIR}/kubectl /usr/local/bin
-tar --exclude='kubectl' --exclude='README.md' -C /usr/local/bin  -zxf ${SOURCES_DIR}/openshift-client-linux-${OC_VERSION}.tar.gz
+tar --exclude='README.md' -C /usr/local/bin  -zxf ${SOURCES_DIR}/openshift-client-linux.tar.gz
 
 chmod +x /usr/local/bin/oc /usr/local/bin/kubectl
--- quarkus-modules/graalvm-ce/install.sh
+++ quarkus-modules/graalvm-ce/install.sh
@@ -8,4 +8,4 @@ SOURCES_DIR=/tmp/artifacts
 
 mkdir -p /opt/graalvm 
 tar -xzf ${SOURCES_DIR}/graalvm-ce-amd64.tar.gz -C /opt/graalvm --strip-components=1
-/opt/graalvm/bin/gu install native-image
+/opt/graalvm/bin/gu --auto-yes install native-image
