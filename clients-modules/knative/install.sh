26970e5c806533436bf2e3bc517d17863c775694
Kamesh Sampath <kamesh.sampath@gmail.com>
2020-05-12 23:54:04 +0530

    upgrade to OpenShift 4.4

--- clients-modules/knative/install.sh
+++ clients-modules/knative/install.sh
@@ -9,4 +9,9 @@ SOURCES_DIR=/tmp/artifacts
 
 cp -v "${SOURCES_DIR}/kn"  /usr/local/bin/kn
 tar  -C /usr/local/bin  -zxf "${SOURCES_DIR}/tkn.tar.gz"
-chmod +x  /usr/local/bin/kn /usr/local/bin/tkn
+tar  -C /usr/local/bin --exclude='kamel.asc' \
+  --exclude='kamel.sha512'  \
+  --exclude='NOTICE' \
+  --exclude='LICENSE' \
+  -zxf "${SOURCES_DIR}/kamel.tar.gz"
+chmod +x  /usr/local/bin/kn /usr/local/bin/tkn /usr/local/bin/kamel
