bffa981941a7423fb9723f3fcd925710c57a3457
Kevin Dubois <kevin.dubois@gmail.com>
2023-06-26 18:23:07 +0200

    update versions

--- clients-modules/knative/install.sh
+++ clients-modules/knative/install.sh
@@ -8,10 +8,14 @@ set -o pipefail
 SOURCES_DIR=/tmp/artifacts
 
 cp -v "${SOURCES_DIR}/kn"  /usr/local/bin/kn
-tar  -C /usr/local/bin  -zxf "${SOURCES_DIR}/tkn.tar.gz"
-tar  -C /usr/local/bin --exclude='kamel.asc' \
+tar -C /usr/local/bin --exclude='kamel.asc' \
   --exclude='kamel.sha512'  \
   --exclude='NOTICE' \
   --exclude='LICENSE' \
   -zxf "${SOURCES_DIR}/kamel.tar.gz"
+tar -C /usr/local/bin \
+  --exclude='LICENSE' \
+  --exclude='README.md' \
+  --no-same-owner \
+  -zxf "${SOURCES_DIR}/tkn.tar.gz"
 chmod +x  /usr/local/bin/kn /usr/local/bin/tkn /usr/local/bin/kamel
